ad0daf1569c8d5ce31b392f2ad42ed0adadf5772
Phyton337 <155496501+phyton337@users.noreply.github.com>
2024-01-04 12:23:41 -0400

    Create Phyton337.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Phyton337.py
@@ -0,0 +1,31 @@
+# https://www.python.org/
+""" 
+Al igual que en otros lenguajes de programación, 
+podemos también comentar varias líneas de código.
+"""
+'''
+Para ello es necesario hacer uso de triples comillas bien sean simples ''' o dobles """. 
+Es necesario usarlas para abrir el bloque del comentario y para cerrarlo.
+'''
+my_variable = "Variablex" # Se puede usar cadenas de texto y numeros para darle valor a una variable
+my_variable = "10" # Se puede dar otro valor a la misma variable
+COSNTANTE = "Constante1" 
+""" 
+No existe ninguna diferencia en Python entre las constantes y variables
+solo como buena práctica se ha asumido nombrarlas en MAYUSCULAS, para 
+asumir que no se debian cambiar.
+"""
+my_int = 7
+my_float = 7.3
+my_bool = True
+my_bool = False
+my_string = "Cadena de texto"
+my_other_string = 'otra cadena de texto'
+
+print("¡Hola, Python!")
+
+print(type(my_int))
+print(type(my_float))
+print(type(my_bool))
+print(type(my_string))
+
