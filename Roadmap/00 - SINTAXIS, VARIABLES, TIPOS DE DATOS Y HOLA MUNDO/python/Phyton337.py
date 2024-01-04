5b1b6286b7efb2b49b9d2f4962226f6ed1e2cfb8
Phyton337 <155496501+phyton337@users.noreply.github.com>
2024-01-04 12:40:59 -0400

    Update Phyton337.py

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Phyton337.py
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Phyton337.py
@@ -10,11 +10,13 @@ Es necesario usarlas para abrir el bloque del comentario y para cerrarlo.
 my_variable = "Variablex" # Se puede usar cadenas de texto y numeros para darle valor a una variable
 my_variable = "10" # Se puede dar otro valor a la misma variable
 COSNTANTE = "Constante1" 
-""" 
-No existe ninguna diferencia en Python entre las constantes y variables
+
+"""
+No existe ninguna diferencia en Phyton entre las constantes y variables
 solo como buena práctica se ha asumido nombrarlas en MAYUSCULAS, para 
 asumir que no se debian cambiar.
 """
+
 my_int = 7
 my_float = 7.3
 my_bool = True
